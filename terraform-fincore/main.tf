d9c1dbe890418a39aab45b2d218f8362dd3166d3
Ankit-goyal-git <goyalankitgoyal32@gmail.com>
2025-08-07 01:38:11 +0530

    SMTP set

--- terraform-fincore/main.tf
+++ terraform-fincore/main.tf
@@ -42,10 +42,42 @@ resource "aws_instance" "fincore_ec2" {
 user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              yum install git -y
+              amazon-linux-extras enable docker
+              yum install -y docker git
+
+              # Start Docker service
+              systemctl enable docker
+              systemctl start docker
+
+              # Allow ec2-user to run Docker
+              usermod -aG docker ec2-user
+              chmod 666 /var/run/docker.sock
+
               cd /home/ec2-user
               git clone https://github.com/darpan-cloud/fincore-project.git
-              chown -R ec2-user:ec2-user fincore-project 
+              chown -R ec2-user:ec2-user fincore-project
+              cd fincore-project
+
+               # Build and run Flask App
+              docker build -t banking-app .
+              docker run -d -p 5000:5000 banking-app
+
+              # Start Prometheus
+              docker run -d -p 9090:9090 \
+                -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml \
+                prom/prometheus
+
+              # Start Node Exporter
+              docker run -d -p 9100:9100 prom/node-exporter
+
+              # Start Grafana with dashboard and alert provisioning
+              docker run -d -p 3000:3000 \
+                -v $(pwd)/grafana/provisioning:/etc/grafana/provisioning \
+                -v $(pwd)/grafana/dashboards:/var/lib/grafana/dashboards \
+                -v $(pwd)/grafana/grafana.ini:/etc/grafana/grafana.ini \
+                -v grafana-storage:/var/lib/grafana \
+                grafana/grafana
+
               EOF
 
   tags = {
